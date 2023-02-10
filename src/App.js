3389db09a9b335bbb3e9eaf96b611808697a8f91
Chaitanya Arora <Tanya242005@gmail.com>
2023-02-10 23:31:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React With Me
-        </a>
-      </header>
+      <p>Green Tech</p>
     </div>
   );
 }
